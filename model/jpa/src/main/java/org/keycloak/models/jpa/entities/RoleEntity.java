afa04f6b064e4873be85cba95a42108e6a9979f9
Axel Messinese <axel.messinese@epfl.ch>
2019-10-15 13:50:45 +0200

    KEYCLOAK-11733 Add endpoint to get parents of a role

--- model/jpa/src/main/java/org/keycloak/models/jpa/entities/RoleEntity.java
+++ model/jpa/src/main/java/org/keycloak/models/jpa/entities/RoleEntity.java
@@ -102,6 +102,9 @@ public class RoleEntity {
     @ManyToMany(fetch = FetchType.LAZY, cascade = {})
     @JoinTable(name = "COMPOSITE_ROLE", joinColumns = @JoinColumn(name = "COMPOSITE"), inverseJoinColumns = @JoinColumn(name = "CHILD_ROLE"))
     private Set<RoleEntity> compositeRoles = new HashSet<>();
+    
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {}, mappedBy = "compositeRoles")
+    private Set<RoleEntity> parentRoles = new HashSet<>();
 
     @OneToMany(cascade = CascadeType.REMOVE, orphanRemoval = true, mappedBy="role")
     @Fetch(FetchMode.SELECT)
@@ -154,6 +157,14 @@ public class RoleEntity {
 
     public void setCompositeRoles(Set<RoleEntity> compositeRoles) {
         this.compositeRoles = compositeRoles;
+    }
+    
+    public Set<RoleEntity> getParentRoles() {
+        return parentRoles;
+    }
+
+    public void setParentRoles(Set<RoleEntity> parentRoles) {
+        this.parentRoles = parentRoles;
     }
 
     public boolean isClientRole() {
